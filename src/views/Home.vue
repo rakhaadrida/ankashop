6d81689af1e5ab9710e2afafc3edb60fff9e4f94
rakhaadrida <rakhaab@gmail.com>
2020-08-06 01:11:11 +0700

    Inisialisasi repo

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <Header />
+    <Hero />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from "@/components/Header.vue";
+import Hero from "@/components/Hero.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Header,
+    Hero,
+  },
+};
+</script>
